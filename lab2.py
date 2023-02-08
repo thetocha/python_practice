f71236af8f0508fd06582bcf8d4a67b5a2d5e2df
Anton Hubko <antongub3@gmail.com>
2023-02-08 21:21:33 +0300

    only one array now in last task

--- lab2.py
+++ lab2.py
@@ -36,22 +36,26 @@ def reorganisation(matrix, x):
     greater = []
     for i, elem in enumerate(matrix):
         if abs(elem) < abs(x):
-            lower.append(elem)
-        else:
-            greater.append(elem)
-    matrix = lower + greater
+            # lower.append(elem)
+            tmp = elem
+            matrix.remove(elem)
+            matrix.insert(index, tmp)
+            index += 1
+        # else:
+        # greater.append(elem)
+    # matrix = lower + greater
     return matrix
 
 
 def create_matrix(size):
     matrix = [0 for i in range(size)]
     for i in range(size):
-            matrix[i] = round(random() * 100 % 11)
+        matrix[i] = round(random() * 100 % 11)
     return matrix
 
 
 if __name__ == '__main__':
-    print(matrix := create_matrix(3))
-    print(max_el(matrix))
-    print(sum_mtr(matrix))
-    print(reorganisation(matrix, 6))
+    print(m := create_matrix(3))
+    print(max_el(m))
+    print(sum_mtr(m))
+    print(reorganisation(m, 6))
